78f0f8a567179f1f33860bf57b839c470afc63c1
Giuseppe <giuseppe.salerno89@gmail.com>
2018-12-14 17:50:02 +0100

    Usable version

--- data_downloader.py
+++ data_downloader.py
@@ -2,12 +2,35 @@ import requests
 import simplejson as js
 from datetime import datetime
 import pystrava
+import numpy as np
+import pandas as pd
+
+print('Start!')
+
+download_all_enabled = False
 
 with open('setup.ini') as fid:
     initial_settings = js.load(fid)
 
 
-strava_session = pystrava.strava_user(base_url = 'https://www.strava.com/api/v3',client_id=initial_settings['client_id'],
+stcon = pystrava.strava_user(base_url = 'https://www.strava.com/api/v3',client_id=initial_settings['client_id'],
                                       client_secret=initial_settings['client_secret'],
                                       code=initial_settings['code'])
+stcon.get_token()
+if download_all_enabled:
+    act_list = stcon.get_activity_list()
+    with open('activity_list.json','w+') as fid:
+        js.dump(act_list,fp=fid)
+else:
+    with open('activity_list.json','r') as fid:
+        act_list = js.load(fp=fid)
+
+act_data = pd.DataFrame(act_list)
+act_data['start_date_utc'] = act_data.start_date.apply(lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ').timestamp())
+
+
+
+a = stcon.get_activity_streams(2006283792)
+
+print('Finish!')
 
--- pystrava.py
+++ pystrava.py
@@ -1,10 +1,11 @@
 import requests
 from datetime import datetime
 import simplejson as js
+import pandas as pd
 
 class strava_user(object):
 
-    def __init__(self,base_url,client_id,client_secret,code):
+    def __init__(self,base_url:str,client_id:str,client_secret:str,code:str):
         '''
 
         :param base_url:
@@ -17,8 +18,7 @@ class strava_user(object):
         self.client_secret = client_secret
         self.code = code
 
-
-    def get_tkn(self)->str:
+    def get_token(self)->str:
         '''
         get token
         :return:
@@ -31,7 +31,10 @@ class strava_user(object):
 
         r = requests.post('https://www.strava.com/oauth/token', params=params)
         tkn = js.loads(r.content)['access_token']
-        self.tkn = tkn
+        self.token = tkn
+        self.std_get_headers = {'Accept': "application/json",
+                                'Authorization': "Bearer " + self.token
+                                }
 
         return tkn
 
@@ -46,10 +49,7 @@ class strava_user(object):
                        "after": 0,
                        'page': 0,
                        'per_page': 200}
-        headers = {
-            'Accept': "application/json",
-            'Authorization': "Bearer " + self.tkn,
-        }
+        headers = self.std_get_headers
         list_completed = False
         idx = 0
         activity_list = []
@@ -60,10 +60,50 @@ class strava_user(object):
             temp_list = js.loads(r.content)
             activity_list = activity_list + temp_list
 
-            if len(activity_list)<200:
+            if len(temp_list)<200:
                 list_completed = True
 
+
         return activity_list
+
+    def get_activity_streams(self,actid_list)->list:
+        '''
+
+        :param actid_list: 2006283792
+        :return:
+        '''
+
+        if isinstance(actid_list,(float,int,str)):
+            actid_list = [str(actid_list)]
+
+        act_stream_list = []
+        for idx, id in enumerate(actid_list):
+
+            url = (self.brl + '/activities/{id}/streams').format(id=id)
+
+            keys_2_get = ['watts','cadence','distance','time','latlng','altitude','velocity_smooth',
+                          'heartrate','temp','moving','grade_smooth']
+            querystring = {"keys":','.join(keys_2_get),"key_by_type":"true"}
+
+            headers = self.std_get_headers
+
+            r = requests.request("GET", url, data='', headers=headers, params=querystring)
+
+            act_stream_list = act_stream_list + [js.loads(r.content)]
+
+        return act_stream_list
+
+    # def create_df_by(self,by='start_date',activity_list=None)->pd.DataFrame:
+    #     '''
+    #
+    #     :param by:
+    #     :param activity_list:
+    #     :return:
+    #     '''
+    #     datetime.strptime(act_list[0]['start_date'], '%Y-%m-%dT%H:%M:%SZ').timestamp()
+    #
+    #     for i in activity_list:
+
 
 
 
