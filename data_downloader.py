e2e3906fd5f65e38c890ae065173aa60c105f924
Giuseppe <giuseppe.salerno89@gmail.com>
2018-12-14 16:12:27 +0100

    Commit test

--- data_downloader.py
+++ data_downloader.py
@@ -10,3 +10,4 @@ with open('setup.ini') as fid:
 strava_session = pystrava.strava_user(base_url = 'https://www.strava.com/api/v3',client_id=initial_settings['client_id'],
                                       client_secret=initial_settings['client_secret'],
                                       code=initial_settings['code'])
+
